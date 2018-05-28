409fc2f338b3b97f8ac6439c9db9633f76298b5c
Alexa Scott <arscott4792@gmail.com>
2018-05-28 12:42:41 -0400

    connected to mLab and tested successfully

--- users/routes.js
+++ users/routes.js
@@ -3,12 +3,16 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../users/model');
+const User = require('./models');
 
 // get users mLab
 //test mLab - url
 
-router.get('/users', (req, res) {
+router.get('/users', (req, res) => {
+  User.find()
+    .then(results => {
+      res.json(results);
+    });
   
 });
 
