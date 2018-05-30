7e6a5402d406e23e606e252c0b606c0a7919303b
Alexa Scott <arscott4792@gmail.com>
2018-05-30 17:59:58 -0400

    updated questions with every letter in alphabet

--- db/questions.js
+++ db/questions.js
@@ -4,40 +4,136 @@
 const questions =
 [
   {
-    'img': 'https://lingojam.com/img/ASL_signs/a.png',
+    'img': 'http://www.clker.com/cliparts/1/2/0/d/1247117058561074247Sign_language_A.svg.hi.png',
     'answer': 'a',
     'memVal': '1'
   },
   {
-    'img': 'https://ih0.redbubble.net/image.171914409.3994/flat,1000x1000,075,f.u1.jpg',
+    'img': 'http://www.clker.com/cliparts/5/2/9/7/12472006341667084845Sign_language_B.svg',
     'answer': 'b',
     'memVal': '1'
   },
   {
-    'img': 'https://i.pinimg.com/originals/3a/5d/35/3a5d355b147e16363bad086eba3ba101.png',
+    'img': 'http://www.clker.com/cliparts/e/4/1/7/12471170721815239278Sign_language_C.svg.med.png',
     'answer': 'c',
     'memVal': '1'
   },
   {
-    'img': 'https://i.pinimg.com/originals/38/93/24/3893244f8251e284d1071a06e57bbc79.png',
+    'img': 'http://www.clker.com/cliparts/2/1/0/9/11954220411982838432liftarn_Sign_language_D_finger_pointing.svg.med.png',
     'answer': 'd',
     'memVal': '1'
   },
   {
-    'img': 'http://www.clker.com/cliparts/1/2/0/d/1247117058561074247Sign_language_A.svg.hi.png',
+    'img': 'http://www.clker.com/cliparts/a/1/5/0/12472006701489259100Sign_language_E.svg',
     'answer': 'e',
     'memVal': '1'
   },
   {
-    'img': 'https://i.pinimg.com/736x/65/f7/a5/65f7a5a6189a64f2b9d6199fc52a0fd3--letter-f-sign-language.jpg',
+    'img': 'http://www.clker.com/cliparts/e/2/8/6/12471170871475869599Sign_language_F.svg.med.png',
     'answer': 'f',
     'memVal': '1'
   },
   {
-    'img': 'http://cliparts.co/cliparts/ki8/oop/ki8oopE5T.png',
+    'img': 'http://www.clker.com/cliparts/f/5/5/b/1247117102653753435Sign_language_G.svg.med.pnghttp://cliparts.co/cliparts/ki8/oop/ki8oopE5T.png',
     'answer': 'g',
     'memVal': '1'
   },
+  {
+    'img': 'http://www.clker.com/cliparts/5/4/a/7/12471171171094804693Sign_language_H.svg.med.png',
+    'answer': 'h',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/f/8/1/7/12471171321180813040Sign_language_I.svg',
+    'answer': 'i',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/3/1/5/5/12471171471724121722Sign_language_J.svg.med.png',
+    'answer': 'j',
+    'memVal': '1'
+  },
+  {
+    'img': 'https://i.pinimg.com/originals/af/1b/03/af1b03c3171ea5a0c12190d07cc61d3c.png',
+    'answer': 'k',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/a/3/2/1/12471171621125803968Sign_language_L.svg.med.png',
+    'answer': 'l',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/d/f/e/a/12472006981970298342Sign_language_M.svg.med.png',
+    'answer': 'm',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/7/6/f/c/1247200716451396668Sign_language_N.svg.med.png',
+    'answer': 'n',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/1/3/e/c/1247117176164159405Sign_language_O.svg',
+    'answer': 'o',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/e/d/c/a/12472007301523625582Sign_language_P.svg.hi.png',
+    'answer': 'p',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/7/7/d/0/1247117193887786539Sign_language_Q.svg.hi.png',
+    'answer': 'q',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/5/1/5/f/1247200756643783662Sign_language_R.svg',
+    'answer': 'r',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/4/9/5/1/1195422024781490459liftarn_Sign_language_S_fist.svg.med.png',
+    'answer': 's',
+    'memVal': '1'
+  },
+  
+  {
+    'img': 'http://www.clker.com/cliparts/b/b/5/d/12471172261039848492Sign_language_T.svg.med.png',
+    'answer': 't',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/2/c/0/0/1247200788178206873Sign_language_U.svg',
+    'answer': 'u',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/a/f/9/1/1247200811944363423Sign_language_V.svg.hi.png',
+    'answer': 'v',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/e/9/c/7/12472008261132433023Sign_language_W.svg',
+    'answer': 'w',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/6/3/9/d/12471172411585979654Sign_language_X.svg.med.png',
+    'answer': 'x',
+    'memVal': '1'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/0/b/5/5/11954220211777442286liftarn_Sign_language_Y_hang_loose.svg.med.png',
+    'answer': 'y',
+    'memVal': '1'
+  },
+  {
+    'img': 'https://i.pinimg.com/originals/01/42/7a/01427af232ab059ed7157de8d9be9c1d.jpg',
+    'answer': 'z',
+    'memVal': '1'
+  }
 ];
 
 module.exports = questions;
