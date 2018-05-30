a2d2cb2e7a06be3663c6c920f1cc46524c8ee4d0
leonardom249 <leonardo.megan@gmail.com>
2018-05-29 23:49:29 -0500

    combined old and new posts to create one post request with if statements to work with client, need to pull and store correct/incorrect count onserver

--- questions/routes.js
+++ questions/routes.js
@@ -43,79 +43,127 @@ router.get('/questions', (req, res, next) => {
 
 // update correct and incorrect for user
 // switching questions
-
-
 router.post('/questions', (req, res, next) => {
   // console.log('req.bod=====', req.body);
-  const { answer, correct, incorrect } = req.body;
-  // console.log('userAnswer', answer);
-  const { username } = req.user;
+  const { correct, incorrect } = req.body;
+  const {username} = req.user;
   const newList = new LinkedList();
   const newQuestionArray=[];
-
+  
   User.findOne({username})
     .then(user => {
+      console.log('firssttt', user);
       user.userQuestionList.map(question=>{
         newList.insertLast(question);
       });
-
-      console.log('this is the list before the user answers the question', JSON.stringify(newList, null, 2));
-      console.log('>>this is the question taken form newList:', JSON.stringify(newList.head.value, null, 2));
-      console.log('>>this is the answer to the question taken from newList:', newList.head.value.answer);
-      console.log('>>this is what the user answer is:', answer);
-
-      if (newList.head.value.answer.toLowerCase() !== answer.toLowerCase()) {
+      // simple(newList);
+      if(correct === user.questionsObj.correct){
         console.log('!!!WRONG. the answer was not correct so lets move it back one');
         newList.insertAt(newList.head.value, 3);
-
-      } 
-      else if (newList.head.value.answer.toLowerCase() === answer.toLowerCase()) {
+      }
+      else if(correct !== user.questionsObj.correct){
         console.log('!!!NICE. the answer was correct so lets move it to back of list');
         newList.insertLast(newList.head.value);
       }
-
       simple(newList);
       console.log('look here to find where the question went?!', JSON.stringify(newList, null, 2));
-
+      
       let currentNode=newList.head;
       while(currentNode!==null){
         newQuestionArray.push(currentNode.value);
         currentNode= currentNode.next;
       }
-      // console.log(newQuestionArray);
-
+  
       user.userQuestionList=newQuestionArray;
       // console.log(user.userQuestionList);
-
+  
       User.updateOne({username}, {$set: {userQuestionList: user.userQuestionList}})
         .then(result => {
           console.log('updating list');
         });
       user.questionsObj.questionHead = user.userQuestionList[0];
       user.questionsObj.questionNext = user.userQuestionList[1];
-
-      // console.log('finaallll', user.questionsObj);
+  
+      console.log('finaallll', user.questionsObj);
       return res.json(user.questionsObj);
     })
     .catch(err => {
       next(err);
     });
-
-  //TODO:
-  // const {correct, incorrect} = req.body
-  // if(user.questionObj.correct !== correct){
-  //   //means incorrect, please change order of linkedlist/array to work
-  //algorithm B that changes so incorrect question appears earlier
-  //put post in here and just change simple to algorithm b
-  // }
-  // else if (correct===user.user.questionObj.correct){
-  //   //means correct, keep going in order
-  // algorithm A where things progress at the correct pace
-  //put post in here and just change simple to algorithm a
-
-// }
-  
+    
 });
+
+// router.post('/questions', (req, res, next) => {
+//   // console.log('req.bod=====', req.body);
+//   const { correct, incorrect } = req.body;
+//   // console.log('userAnswer', answer);
+//   const { username } = req.user;
+//   const newList = new LinkedList();
+//   const newQuestionArray=[];
+
+//   User.findOne({username})
+//     .then(user => {
+//       user.userQuestionList.map(question=>{
+//         newList.insertLast(question);
+//       });
+
+//       console.log('this is the list before the user answers the question', JSON.stringify(newList, null, 2));
+//       console.log('>>this is the question taken form newList:', JSON.stringify(newList.head.value, null, 2));
+//       console.log('>>this is the answer to the question taken from newList:', newList.head.value.answer);
+//       console.log('>>this is what the user answer is:', answer);
+
+//       if (newList.head.value.answer.toLowerCase() !== answer.toLowerCase()) {
+//         console.log('!!!WRONG. the answer was not correct so lets move it back one');
+//         newList.insertAt(newList.head.value, 3);
+
+//       } 
+//       else if (newList.head.value.answer.toLowerCase() === answer.toLowerCase()) {
+//         console.log('!!!NICE. the answer was correct so lets move it to back of list');
+//         newList.insertLast(newList.head.value);
+//       }
+
+//       simple(newList);
+//       console.log('look here to find where the question went?!', JSON.stringify(newList, null, 2));
+
+//       let currentNode=newList.head;
+//       while(currentNode!==null){
+//         newQuestionArray.push(currentNode.value);
+//         currentNode= currentNode.next;
+//       }
+//       // console.log(newQuestionArray);
+
+//       user.userQuestionList=newQuestionArray;
+//       // console.log(user.userQuestionList);
+
+//       User.updateOne({username}, {$set: {userQuestionList: user.userQuestionList}})
+//         .then(result => {
+//           console.log('updating list');
+//         });
+//       user.questionsObj.questionHead = user.userQuestionList[0];
+//       user.questionsObj.questionNext = user.userQuestionList[1];
+
+//       // console.log('finaallll', user.questionsObj);
+//       return res.json(user.questionsObj);
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+
+//   //TODO:
+//   // const {correct, incorrect} = req.body
+//   // if(user.questionObj.correct !== correct){
+//   //   //means incorrect, please change order of linkedlist/array to work
+//   //algorithm B that changes so incorrect question appears earlier
+//   //put post in here and just change simple to algorithm b
+//   // }
+//   // else if (correct===user.user.questionObj.correct){
+//   //   //means correct, keep going in order
+//   // algorithm A where things progress at the correct pace
+//   //put post in here and just change simple to algorithm a
+
+// // }
+  
+// });
 
 
 module.exports = router;
--- questions/scratch.js
+++ questions/scratch.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//old:
 router.post('/questions', (req, res, next) => {
   // console.log('req.bod=====', req.body);
   const {userAnswer} = req.body;
@@ -53,3 +53,77 @@ router.post('/questions', (req, res, next) => {
   // ...
     
 });
+
+//new:
+router.post('/questions', (req, res, next) => {
+  // console.log('req.bod=====', req.body);
+  const { answer, correct, incorrect } = req.body;
+  // console.log('userAnswer', answer);
+  const { username } = req.user;
+  const newList = new LinkedList();
+  const newQuestionArray=[];
+  
+  User.findOne({username})
+    .then(user => {
+      user.userQuestionList.map(question=>{
+        newList.insertLast(question);
+      });
+  
+      console.log('this is the list before the user answers the question', JSON.stringify(newList, null, 2));
+      console.log('>>this is the question taken form newList:', JSON.stringify(newList.head.value, null, 2));
+      console.log('>>this is the answer to the question taken from newList:', newList.head.value.answer);
+      console.log('>>this is what the user answer is:', answer);
+  
+      if (newList.head.value.answer.toLowerCase() !== answer.toLowerCase()) {
+        console.log('!!!WRONG. the answer was not correct so lets move it back one');
+        newList.insertAt(newList.head.value, 3);
+  
+      } 
+      else if (newList.head.value.answer.toLowerCase() === answer.toLowerCase()) {
+        console.log('!!!NICE. the answer was correct so lets move it to back of list');
+        newList.insertLast(newList.head.value);
+      }
+  
+      simple(newList);
+      console.log('look here to find where the question went?!', JSON.stringify(newList, null, 2));
+  
+      let currentNode=newList.head;
+      while(currentNode!==null){
+        newQuestionArray.push(currentNode.value);
+        currentNode= currentNode.next;
+      }
+      // console.log(newQuestionArray);
+  
+      user.userQuestionList=newQuestionArray;
+      // console.log(user.userQuestionList);
+  
+      User.updateOne({username}, {$set: {userQuestionList: user.userQuestionList}})
+        .then(result => {
+          console.log('updating list');
+        });
+      user.questionsObj.questionHead = user.userQuestionList[0];
+      user.questionsObj.questionNext = user.userQuestionList[1];
+  
+      // console.log('finaallll', user.questionsObj);
+      return res.json(user.questionsObj);
+    })
+    .catch(err => {
+      next(err);
+    });
+  
+  //TODO:
+  // const {correct, incorrect} = req.body
+  // if(user.questionObj.correct !== correct){
+  //   //means incorrect, please change order of linkedlist/array to work
+  //algorithm B that changes so incorrect question appears earlier
+  //put post in here and just change simple to algorithm b
+  // }
+  // else if (correct===user.user.questionObj.correct){
+  //   //means correct, keep going in order
+  // algorithm A where things progress at the correct pace
+  //put post in here and just change simple to algorithm a
+  
+  // }
+    
+});
+  
