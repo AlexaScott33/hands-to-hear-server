3903ec728c90165944fb209d2ec2ff2d8c00ce6b
Alexa Scott <arscott4792@gmail.com>
2018-05-30 10:47:15 -0400

    added dummy data to questions array

--- db/questions.js
+++ db/questions.js
@@ -14,7 +14,23 @@ const questions =
   {
     'img': 'https://i.pinimg.com/originals/3a/5d/35/3a5d355b147e16363bad086eba3ba101.png',
     'answer': 'c'
-  }
+  },
+  {
+    'img': 'https://i.pinimg.com/originals/38/93/24/3893244f8251e284d1071a06e57bbc79.png',
+    'answer': 'd'
+  },
+  {
+    'img': 'http://www.clker.com/cliparts/1/2/0/d/1247117058561074247Sign_language_A.svg.hi.png',
+    'answer': 'e'
+  },
+  {
+    'img': 'https://i.pinimg.com/736x/65/f7/a5/65f7a5a6189a64f2b9d6199fc52a0fd3--letter-f-sign-language.jpg',
+    'answer': 'f'
+  },
+  {
+    'img': 'http://cliparts.co/cliparts/ki8/oop/ki8oopE5T.png',
+    'answer': 'g'
+  },
 ];
 
 module.exports = questions;
